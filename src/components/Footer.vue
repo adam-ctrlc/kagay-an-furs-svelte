22686eb1b6e8c8d2a1cb7d22a70286689eb6c60b
jyn626 <tuyor.jynn@gmail.com>
2026-05-26 16:05:36 +0800

    feat: add back to top button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-primary border-t-2 border-border-base py-12">
+  <footer class="bg-primary/90 border-t-2 border-border-base py-12">
     <div
       class="max-w-5xl mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-3 gap-10"
     >
